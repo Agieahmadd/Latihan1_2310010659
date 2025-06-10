b27dfc7cff836af90f281a302a4ae669df39ed4a
Agieahmadd <agieaeae@gmail.com>
2025-06-10 12:51:59 +0800

    membuat consructor dan set penjumlahan

--- Matematika.java
+++ Matematika.java
@@ -1,3 +1,12 @@
-public class Matematika{
+public class Matematika {
+    private double bill1, bill2;
 
+    public Matematika(double bill1, double bill2) {
+        this.bill1 = bill1;
+        this.bill2 = bill2;
+    }
+
+    public double getPenjumlahan() {
+        return bill1 + bill2;
+    }
 }
